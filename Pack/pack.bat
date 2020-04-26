c0820c375030295e19dddacc9ab60df71b693a07
Chris <chris@tracktownsoftware.com>
2020-04-26 12:25:45 -0700

    Clean up and file name changes

--- Pack/pack.bat
+++ Pack/pack.bat
@@ -1,4 +1,5 @@
 set PACKAGEVERSION=1.2.44
 msbuild ..\WPFControlNetCore\WPFControlNetCore.csproj -p:Configuration=Release 
 msbuild ..\WPFControlNetCore.DesignTools\WPFControlNetCore.DesignTools.csproj -p:Configuration=Release 
+msbuild ..\WPFControlNetCore.ConsoleApp\WPFControlNetCore.ConsoleApp.csproj -p:Configuration=Release 
 dotnet pack -p:PackageVersion=%PACKAGEVERSION% ..\WPFControlNetCore\WPFControlNetCore.csproj --configuration=Release --output Packages
